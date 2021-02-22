b7a8047f76cf8f1d59c43d74e3649665b5a71eef
fuhranku <frankponte95@gmail.com>
2021-02-22 17:24:02 -0400

    CU-m934yw[Completed] - Create a service type page.

--- service-type.php
+++ service-type.php
@@ -10,24 +10,43 @@ get_header();
 ?>
 
 <!-- Main content -->
-<!-- Blog post cover -->
-<div class="min-vh-100 py-9 py-lg-0 bg-cover bg-size--cover" style="background-image: url(../../assets/img/theme/light/blog-hero-1.jpg);"></div>
-<section class="section-half-rounded bg-transparent pt-6 pb-10 mt-n17 zindex-100">
-    <!-- Jumbotron shape -->
-    <div class="jumbotron section-inner left-0 rounded-top-right bg-primary overflow-hidden col-xl-10 col-lg-11"></div>
-    <!-- Container -->
-    <div class="container">
-        <div class="row row-grid align-items-center">
-            <div class="col-xl-9 col-lg-11 offset-lg-1 offset-xl-2">
-                <a href="#" class="badge badge-warning badge-pill">Bootstrap</a>
-                <!-- Heading -->
-                <h1 class="h2 text-white mt-3 mb-6 pr-5">
-                    Keep your face always toward the sunshine - and shadows will fall behind you.
-                </h1>
+    <!-- Heading -->
+    <?php if(have_rows('heading')) : 
+        while (have_rows('heading')) : the_row();    
+    ?>
+    <!-- Check if this section display is enabled -->
+    <?php if (get_sub_field('display')) : ?>
+        <div 
+            class="min-vh-100 py-9 py-lg-0 bg-cover bg-size--cover" 
+            style="background-image: url(<?php the_sub_field('background_image') ?>);"
+        ></div>
+        <section class="section-half-rounded bg-transparent pt-6 pb-10 mt-n17 zindex-100">
+            <!-- Jumbotron shape -->
+            <div class="jumbotron section-inner left-0 rounded-top-right bg-primary overflow-hidden col-xl-10 col-lg-11"></div>
+            <!-- Container -->
+            <div class="container">
+                <div class="row row-grid align-items-center">
+                    <div class="col-xl-9 col-lg-11 offset-lg-1 offset-xl-2">
+                        <a href="#" class="badge badge-warning badge-pill">
+                            <?php the_sub_field('tag')?>
+                        </a>
+                        <!-- Heading -->
+                        <h1 class="h2 text-white mt-3 mb-6 pr-5">
+                            <?php the_sub_field('service_name')?>
+                        </h1>
+                    </div>
+                </div>
             </div>
-        </div>
-    </div>
-</section>
+        </section>
+    <?php endif;?>
+    <?php
+        endwhile;
+    else: 
+    ?>
+        <div> Please check your ACF config. Something went wrong!</div>
+    <?php endif;?>
+
+<!-- Content Slice -->
 <section class="section-half-rounded bg-transparent py-5 mt-n10 zindex-101">
     <!-- Jumbotron shape -->
     <div class="jumbotron section-inner right-0 rounded-0 overflow-hidden col-xl-10 col-lg-12"></div>
@@ -99,52 +118,6 @@ get_header();
                     <p>
                         Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
                     </p>
-                    <!-- Testimonials -->
-                    <div class="card bg-dark my-5">
-                        <div class="card-body py-5 px-lg-5 swiper-js-container">
-                            <div class="swiper-container" data-swiper-items="1" data-swiper-space-between="0">
-                                <div class="swiper-wrapper">
-                                    <div class="swiper-slide">
-                                        <div class="text-center">
-                                            <p class="h4 lh-180 px-4 font-weight-300 text-white">
-                                                "Amazing looking theme and instantly turns your application into a professional looking one. Really shows that professionals have built this theme up. Very happy with the way the theme looks and the example
-                                                pages are very good."
-                                            </p>
-                                            <div class="text-center mt-4">
-                                                <span class="h6 font-weight-light text-white opacity-8">Saif</span>
-                                                <span class="h6 text-warning"> - Bootstrap Marketplace</span>
-                                            </div>
-                                        </div>
-                                    </div>
-                                    <div class="swiper-slide">
-                                        <div class="text-center">
-                                            <p class="h4 lh-180 px-4 font-weight-300 text-white">
-                                                "I have worked with different themes, but this one stands out in terms of beautiful organization. The theme also has many example pages and a host of components to choose from. Can't thank you enough!"
-                                            </p>
-                                            <div class="text-center mt-4">
-                                                <span class="h6 font-weight-light text-white">Nelson</span>
-                                                <span class="h6 text-warning"> - Bootstrap Marketplace</span>
-                                            </div>
-                                        </div>
-                                    </div>
-                                    <div class="swiper-slide">
-                                        <div class="text-center">
-                                            <p class="h4 lh-180 px-4 font-weight-300 text-white">
-                                                "What I really appreciate about this <strong>Pack</strong> is the fact that it contains hundreds of different opportunities to create beautiful webpages. I would start use this more and more!"
-                                            </p>
-                                            <div class="text-center mt-4">
-                                                <span class="h6 font-weight-light text-white">Mathias</span>
-                                                <span class="h6 text-warning"> - Bootstrap Marketplace</span>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                            <!-- Add Arrows -->
-                            <div class="swiper-button swiper-button-next btn-dark zindex-101"></div>
-                            <div class="swiper-button swiper-button-prev btn-dark zindex-101"></div>
-                        </div>
-                    </div>
                     <!-- Heading -->
                     <h5>2. This is a secondary heading</h5>
                     <!-- Text -->
@@ -215,210 +188,6 @@ get_header();
                         </div>
                     </div>
                 </article>
-                <!-- Share -->
-                <div class="row align-items-center mt-7 py-4 border-top border-bottom">
-                    <div class="col mb-3 mb-lg-0">
-                        <div class="media align-items-center">
-                            <div>
-                                <a href="#" class="avatar rounded-circle mr-3">
-                                    <img alt="Image placeholder" src="../../assets/img/theme/light/person-2.jpg">
-                                </a>
-                            </div>
-                            <div class="media-body">
-                                <span class="d-block h6 mb-0">John Sullivan</span>
-                                <span class="text-sm text-muted">Published on July 25, 2019</span>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-auto">
-                        <ul class="list-inline mb-0">
-                            <li class="list-inline-item pr-4">
-                                <a href="#" class="text-muted"><i data-feather="share" class="mr-1 text-muted"></i> 131</a>
-                            </li>
-                            <li class="list-inline-item pr-4">
-                                <a href="#" class="text-muted"><i data-feather="eye" class="mr-1 text-muted"></i> 255</a>
-                            </li>
-                            <li class="list-inline-item">
-                                <a href="#" class="text-muted"><i data-feather="message-circle" class="mr-1 text-muted"></i> 14</a>
-                            </li>
-                        </ul>
-                    </div>
-                </div>
-                <!-- Comments -->
-                <div class="mt-5">
-                    <h6 class="mb-4">Comments</h6>
-                    <!-- Comment entry -->
-                    <div class="media media-comment">
-                        <img alt="Image placeholder" class="rounded-circle shadow mr-4" src="../../assets/img/theme/light/person-3.jpg" style="width: 58px;">
-                        <div class="media-body">
-                            <div class="media-comment-bubble left-top">
-                                <h6 class="mt-0">Alexis Ren</h6>
-                                <p class="text-sm lh-160">Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis.</p>
-                                <div class="icon-actions">
-                                    <a href="#" class="love active">
-                                        <i data-feather="heart"></i>
-                                        <span class="text-muted">10 likes</span>
-                                    </a>
-                                    <a href="#">
-                                        <i data-feather="message-circle"></i>
-                                        <span class="text-muted">1 reply</span>
-                                    </a>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="media media-comment">
-                        <img alt="Image placeholder" class="rounded-circle shadow mr-4" src="../../assets/img/theme/light/person-4.jpg" style="width: 58px;">
-                        <div class="media-body">
-                            <div class="media-comment-bubble left-top">
-                                <h6 class="mt-0">Tom Cruise</h6>
-                                <p class="text-sm lh-160">Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis.</p>
-                                <div class="icon-actions">
-                                    <a href="#" class="love active">
-                                        <i data-feather="heart"></i>
-                                        <span class="text-muted">20 likes</span>
-                                    </a>
-                                    <a href="#">
-                                        <i data-feather="message-circle"></i>
-                                        <span class="text-muted">3 replies</span>
-                                    </a>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="media media-comment align-items-center">
-                        <img alt="Image placeholder" class="rounded-circle shadow mr-4" src="../../assets/img/theme/light/person-1.jpg" style="width: 58px;">
-                        <div class="media-body">
-                            <form class="rounded-lg border">
-                                <div class="input-group input-group-lg input-group-merge">
-                                    <!-- Prepend -->
-                                    <div class="input-group-prepend">
-                                        <span class="input-group-text border-0 pr-2">
-                                            <i data-feather="edit-2"></i>
-                                        </span>
-                                    </div>
-                                    <!-- Input -->
-                                    <input type="text" class="form-control border-0 px-1" aria-label="Find something" placeholder="Write a comment...">
-                                    <!-- Append -->
-                                    <div class="input-group-append">
-                                        <span class="input-group-text border-0 py-0 pl-2 pr-2">
-                                            <button type="button" class="btn btn-sm btn-primary">Post</button>
-                                        </span>
-                                    </div>
-                                </div>
-                            </form>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-    <!-- SVG separator -->
-    <div class="shape-container shape-position-bottom">
-        <img alt="Image placeholder" src="../../assets/img/svg/separators/line.svg" class="svg-inject fill-section-secondary">
-    </div>
-</section>
-<section class="slice slice-lg bg-section-secondary">
-    <div class="container">
-        <!-- Title -->
-        <div class="row align-items-center mb-5">
-            <div class="col-12 col-md">
-                <h3 class="h4 mb-0">Keep reading</h3>
-                <p class="mb-0 text-muted">
-                    Even more stories from Quick
-                </p>
-            </div>
-            <div class="col-12 col-md-auto">
-                <a href="#!" class="btn btn-sm btn-neutral d-none d-md-inline">
-                    View all
-                </a>
-            </div>
-        </div>
-        <!-- Posts -->
-        <div class="row">
-            <div class="col-xl-4 col-md-6">
-                <div class="card hover-translate-y-n3 hover-shadow-lg overflow-hidden">
-                    <div class="position-relative overflow-hidden">
-                        <a href="#" class="d-block">
-                            <img alt="Image placeholder" src="../../assets/img/theme/light/blog-1-800x600.jpg" class="card-img-top">
-                        </a>
-                    </div>
-                    <div class="card-body py-4">
-                        <small class="d-block text-sm mb-2">25 April, 2020</small>
-                        <a href="#" class="h5 stretched-link lh-150">Choose the best solution for your business</a>
-                        <p class="mt-3 mb-0 lh-170">No matter what he does, every person on earth plays a central role in the history.</p>
-                    </div>
-                    <div class="card-footer border-0 delimiter-top">
-                        <div class="row align-items-center">
-                            <div class="col-auto">
-                                <span class="avatar avatar-sm bg-primary rounded-circle">JD</span>
-                                <span class="text-sm mb-0 avatar-content">David Wally</span>
-                            </div>
-                            <div class="col text-right text-right">
-                                <div class="actions">
-                                    <a href="#" class="action-item"><i data-feather="heart" class="mr-1"></i> 50</a>
-                                    <a href="#" class="action-item"><i data-feather="eye" class=" mr-1"></i> 250</a>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <div class="col-xl-4 col-md-6">
-                <div class="card hover-translate-y-n3 hover-shadow-lg overflow-hidden">
-                    <div class="position-relative overflow-hidden">
-                        <a href="#" class="d-block">
-                            <img alt="Image placeholder" src="../../assets/img/theme/light/blog-2-800x600.jpg" class="card-img-top">
-                        </a>
-                    </div>
-                    <div class="card-body py-4">
-                        <small class="d-block text-sm mb-2">25 April, 2020</small>
-                        <a href="#" class="h5 stretched-link lh-150">How to find the right design for your specific product</a>
-                        <p class="mt-3 mb-0 lh-170">No matter what he does, every person on earth plays a central role in the history.</p>
-                    </div>
-                    <div class="card-footer border-0 delimiter-top">
-                        <div class="row align-items-center">
-                            <div class="col-auto">
-                                <span class="avatar avatar-sm bg-primary rounded-circle">JD</span>
-                                <span class="text-sm mb-0 avatar-content">David Wally</span>
-                            </div>
-                            <div class="col text-right text-right">
-                                <div class="actions">
-                                    <a href="#" class="action-item"><i data-feather="heart" class="mr-1"></i> 50</a>
-                                    <a href="#" class="action-item"><i data-feather="eye" class=" mr-1"></i> 250</a>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <div class="col-xl-4 col-md-6">
-                <div class="card hover-translate-y-n3 hover-shadow-lg overflow-hidden">
-                    <div class="position-relative overflow-hidden">
-                        <a href="#" class="d-block">
-                            <img alt="Image placeholder" src="../../assets/img/theme/light/blog-3-800x600.jpg" class="card-img-top">
-                        </a>
-                    </div>
-                    <div class="card-body py-4">
-                        <small class="d-block text-sm mb-2">25 April, 2020</small>
-                        <a href="#" class="h5 stretched-link lh-150">Win buyers and influence sales with marketing</a>
-                        <p class="mt-3 mb-0 lh-170">No matter what he does, every person on earth plays a central role in the history.</p>
-                    </div>
-                    <div class="card-footer border-0 delimiter-top">
-                        <div class="row align-items-center">
-                            <div class="col-auto">
-                                <span class="avatar avatar-sm bg-primary rounded-circle">JD</span>
-                                <span class="text-sm mb-0 avatar-content">David Wally</span>
-                            </div>
-                            <div class="col text-right text-right">
-                                <div class="actions">
-                                    <a href="#" class="action-item"><i data-feather="heart" class="mr-1"></i> 50</a>
-                                    <a href="#" class="action-item"><i data-feather="eye" class=" mr-1"></i> 250</a>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
             </div>
         </div>
     </div>
